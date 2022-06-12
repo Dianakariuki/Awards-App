7e88bc09262f3ed1bbfa2a270e529c4a8f595a4d
Diana Kariuki <dianakariuki842@gmail.com>
2022-06-12 22:30:30 +0300

    This commit creates Data base relation ships

--- awards/models.py
+++ awards/models.py
@@ -3,4 +3,16 @@ from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator, MinValueValidato
 
 # Create your models here.
+
 class Projects(models.Model):
+    name=models.CharField(max_length=30)
+    image=models.ImageField(upload_to='projects/')
+    design=models.IntegerField(default=0)
+    usability=models.IntegerField(default=0)
+    content=models.IntegerField(default=0)
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    description=models.TextField(max_length=320)
+    link=models.URLField(max_length=60)
+    date=models.DateField(auto_now=True)
+    screen1=models.ImageField(upload_to='screenshot/',blank=True)
+    screen2=models.ImageField(upload_to='screenshot/',blank=True)
