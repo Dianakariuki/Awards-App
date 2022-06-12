91269fa72070303ea31aeb9fc5a61284580be744
Diana Kariuki <dianakariuki842@gmail.com>
2022-06-12 23:59:49 +0300

    This commit installs django registration

--- awards/urls.py
+++ awards/urls.py
@@ -1,5 +1,5 @@
 from . import views
-from django.conf.urls import url
+from django.urls import re_path
 from django.conf.urls.static import static
 from django.conf import settings
 from rest_framework.authtoken.views import obtain_auth_token
@@ -8,16 +8,16 @@ from rest_framework.authtoken.views import obtain_auth_token
 #url urlpatterns
 
 urlpatterns=[
-    url(r'^$',views.index, name='index'),
-    url(r'project/post/$',views.post,name='post'),
-    url(r'^user/profile/$',views.profile,name='profile'),
-    url(r'^project/(\d+)/',views.project_detail,name='details'),
-    url(r'^search/projects/results/$',views.search,name="search"),
-    # url(r'^ajax/review/(\d+)$',views.ajaxRequest,name='review'),
-    url(r'^api/projects/$',views.ProjectList.as_view()),
-    url(r'^api/profile/$',views.ProfileList.as_view()),
-    url(r'^token/', obtain_auth_token),
-    url(r'^developer/api/$',views.apiView,name='api'),
+    re_path(r'^$',views.index, name='index'),
+    re_path(r'project/post/$',views.post,name='post'),
+    re_path(r'^user/profile/$',views.profile,name='profile'),
+    re_path(r'^project/(\d+)/',views.project_detail,name='details'),
+    re_path(r'^search/projects/results/$',views.search,name="search"),
+    # re_path(r'^ajax/review/(\d+)$',views.ajaxRequest,name='review'),
+    re_path(r'^api/projects/$',views.ProjectList.as_view()),
+    re_path(r'^api/profile/$',views.ProfileList.as_view()),
+    re_path(r'^token/', obtain_auth_token),
+    re_path(r'^developer/api/$',views.apiView,name='api'),
 ]
 
 if settings.DEBUG:
--- projects/urls.py
+++ projects/urls.py
@@ -13,14 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url,include
+from django.urls import re_path,include
 from django.contrib import admin
 from django.contrib.auth import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^',include('award.urls')),
-    url(r'accounts/',include('registration.backends.simple.urls')),
-    url(r'^accounts/', include('django.contrib.auth.urls')),
-    url(r'^logout/',views.logout,{'next_page':'/'}),
+    re_path(r'^admin/', admin.site.urls),
+    re_path(r'^',include('awards.urls')),
+    re_path(r'accounts/',include('registration.backends.simple.urls')),
+    re_path(r'^accounts/', include('django.contrib.auth.urls')),
+    re_path(r'^logout/',views.logout,{'next_page':'/'}),
 ]
