08d215d6888c810c52ff1d14904e34edecf9221e
Diana Kariuki <dianakariuki842@gmail.com>
2022-06-12 22:43:39 +0300

    This commit creates functions in the views.py

--- awards/views.py
+++ awards/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import PostForm,RateForm,ReviewForm,UpdateForm
+from .models import Projects,Rates,Comments,Profile
+from django.http import HttpResponse,Http404,HttpResponseRedirect,JsonResponse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializer import ProjectSerializer,ProfileSerializer
 
 # Create your views here.
+def index(request):
+
+    try:
+        projects=Projects.objects.all()
+    except Exception as e:
+        raise  Http404()
+    return render(request,'index.html',{"projects":projects})
