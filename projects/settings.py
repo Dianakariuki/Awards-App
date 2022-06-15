726d90608082617e075c7451c01b4166a45cbdf8
Diana Kariuki <dianakariuki842@gmail.com>
2022-06-15 06:41:44 +0300

    This commit solves errors on deployment

--- projects/settings.py
+++ projects/settings.py
@@ -114,7 +114,7 @@ else:
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+ALLOWED_HOSTS = config('ALLOWED_HOSTS','diana-awards.herokuapp.com', cast=Csv())
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
